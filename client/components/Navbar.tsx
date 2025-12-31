a4afeb130eb4d5c8ad15b8633c85cce4eeb0bf67
Feven-Tolosa <tolosafeven0@gmail.com>
2025-12-31 22:23:16 +0300

    nav bare

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,34 @@
+'useclient'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <header className='border-b bg-amber-50'>
+      <nav className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
+        {/* Logo */}
+        <Link
+          href='/'
+          className='text-2xl font-bold text-[var(--wood)] hover:text-[var(--wood-dark)]'
+        >
+          TF
+        </Link>
+
+        {/* Links */}
+        <div className='space-x-6 text-sm font-medium'>
+          <Link href='/' className='text-[var(--wood)] hover:underline'>
+            Categories
+          </Link>
+          <Link href='/' className='text-[var(--wood)] hover:underline'>
+            Order
+          </Link>
+          <Link
+            href='/admin/login'
+            className='bg-[var(--wood)] text-white px-4 py-2 rounded hover:bg-[var(--wood-dark)]'
+          >
+            Admin
+          </Link>
+        </div>
+      </nav>
+    </header>
+  )
+}
