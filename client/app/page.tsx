dc4b0111b1f5ef74fe584da4b5a6225b914ea2d2
Feven-Tolosa <tolosafeven0@gmail.com>
2026-02-09 23:57:06 +0300

    unwanted comments removed

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -20,24 +20,6 @@ const furniture = [
 export default function Home() {
   return (
     <>
-      {/* Hero Section */}
-      {/* <section className='bg-gray-50 py-20 mt-15 min-h-screen bg-cover bg-center bg-[url("/images/2.png")] flex items-center'>
-        <div className='max-w-7xl mx-auto px-6 text-center'>
-          <h1 className='text-4xl md:text-5xl font-bold mb-6  '>
-            Crafted Furniture for Everyday Living
-          </h1>
-          <p className=' max-w-2xl mx-auto mb-8'>
-            Discover quality wooden furniture made with care, durability, and
-            timeless design.
-          </p>
-          <a
-            href='#furniture'
-            className='inline-block bg-[var(--wood)] text-white px-6 py-3 rounded hover:bg-[var(--wood-dark)]'
-          >
-            Browse Furniture
-          </a>
-        </div>
-      </section> */}
       <Hero />
       {/* Furniture Section */}
       <section id='furniture' className='max-w-7xl mx-auto px-6 py-20'>
