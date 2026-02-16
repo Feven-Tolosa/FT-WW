b00f040bfa0e820d3150ef25f51a8a865f470427
Feven-Tolosa <tolosafeven0@gmail.com>
2026-02-17 00:05:18 +0300

    horizontal product added

---
+++ client/components/components/HorizontalProducts.tsx
@@ -0,0 +1,31 @@
+import FurnitureCard from '../FurnitureCard'
+interface Props {
+  items: {
+    id: string
+    name: string
+    price: number
+    image: string
+  }[]
+}
+
+export default function HorizontalProducts({ items }: Props) {
+  return (
+    <div className='relative'>
+      <div
+        className='
+          flex gap-6
+          overflow-x-auto
+          scroll-smooth
+          pb-4
+          no-scrollbar
+        '
+      >
+        {items.map((item) => (
+          <div key={item.id} className='min-w-[260px]'>
+            <FurnitureCard name={item.name} image={item.image} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
